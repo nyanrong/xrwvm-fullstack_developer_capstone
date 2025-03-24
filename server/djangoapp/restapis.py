e5cae18997dfd03f045c271f03271d6349085203
nyanrong <nyrlynette@gmail.com>
2025-03-24 14:46:12 -0400

    completed module 4

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,5 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -22,9 +22,9 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as err:
         # If any error occurs
-        print("Network exception occurred")
+        print(f"Network exception occurred - error: {err}")
 
 
 def analyze_review_sentiments(text):
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -97,6 +97,7 @@ def registration(request):
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
 def get_dealerships(request, state="All"):
+    print(state)
     if(state == "All"):
         endpoint = "/fetchDealers"
     else:
